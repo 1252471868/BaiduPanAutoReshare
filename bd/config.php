0197ba29b4eae90899874acc896acf0989581b3c
1252471868 <daixiaoyi123@163.com>
2016-07-29 21:04:39 +0800

    Create config.php

---
+++ bd/config.php
@@ -0,0 +1,26 @@
+<?php
+//mysql
+$host='localhost';
+$user='root';
+$pass='';
+$db='budang';
+
+//要模仿的浏览器
+$ua='netdisk;4.6.1.0;PC;PC-Windows;6.2.9200;WindowsBaiduYunGuanJia';
+
+//后台显示的跳转地址
+$jumper = 'https://github.com/NijiharaTsubasa/BaiduPanAutoReshare/blob/master/bd/jump.php';
+
+//高速下载链接的开关，这些链接只能供（和服务器IP是）同一个国家的IP使用
+$enable_high_speed_link = true;
+
+//直链功能的开关
+$enable_direct_link = true;
+
+//直接播放视频功能的开关【跳转页需要使用HTTPS方可播放】
+$enable_direct_video_play = false;
+
+//生成新文件名，不含扩展名
+function generateNewName() {
+	return '[GalACG]EX' . str_pad((time() - 1402761600),9,'0',STR_PAD_LEFT);
+}
